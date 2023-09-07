9f451ac5215b5634741ff044c47e89254b89ea76
Diyor Safarov <tankisttanjig@gmail.com>
2023-09-07 20:10:49 +0300

    feat: connectTimeout on orders routes

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import { getOrders, denyOrder, acceptOrder } from '../controllers/orderController.js';
 import JWT from '../providers/JWT.js';
+import connectTimeout from 'connect-timeout';
 
 const jwt = new JWT();
 
 const router = express.Router();
 
 router.post('/order-create', jwt.authenticateToken, getOrders);
-router.post('/order-deny', jwt.authenticateToken, denyOrder);
-router.post('/order-accept', jwt.authenticateToken, acceptOrder);
+router.post('/order-deny', jwt.authenticateToken, connectTimeout('10s'), denyOrder);
+router.post('/order-accept', jwt.authenticateToken, connectTimeout('10s'), acceptOrder);
 router.get('/orders/:userId', jwt.authenticateToken);
 
 export default router;
